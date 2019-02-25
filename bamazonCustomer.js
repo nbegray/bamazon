04b71ad5142eb36eeacfdb51ec5b39081a510b02
Natalie Edmonson <natalieedmonson@Natalies-MacBook-Air.local>
2019-02-24 22:38:10 -0700

    finished code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,78 +1,153 @@
-var mysql = require("mysql")
-var inquirer = require("inquirer");
+const mysql = require("mysql")
+const inquirer = require("inquirer");
 const cTable = require('console.table');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "root",
-  database: "bamazon"
+    // Your password
+    password: "root",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  start();
+connection.connect(function (err) {
+    if (err) throw err;
+    startPrompt();
 });
+//========================Inquirer Introduction==================//
 
+function startPrompt() {
 
-function start(){
-    connection.query("SELECT * FROM products", function(err, result){
-      
+    inquirer.prompt([{
+
+        type: "confirm",
+        name: "confirm",
+        message: "Welcome to Bamazon! Would you like to view our inventory?",
+        default: true
+
+    }]).then(function(user) {
+        if (user.confirm === true) {
+
+            startInventory();
+            
+        } else {
+            console.log("Thank you! Come back soon!");
+            startPrompt();
+            
+        
+        }
+    });
+}
+
+//============================Inventory Prompt=======================//
+function startInventory() {
+    connection.query("SELECT * FROM products", function (err, result) {
+        
         console.table(result);
 
-        // prompt the user via inquire
-        // from the propmt we need the id and quantity of the product
-        inquirer
-    .prompt([
+        selectionPrompt();
+       
+    });
+}
+//=============================User Selects # of products==========//
+
+function selectionPrompt() {
+
+    inquirer.prompt([{
+
+            type: "input",
+            name: "inputId",
+            message: "Please enter the ID number of the item you would like to purchase.",
+        },
         {
-      name: "id",
-      type: "input",
-      message: "What id?"
-    },
-    {
-        name: "quantity",
-        type: "input",
-        message: "how many?"
-      },
-    ]).then(function(ans){
-        console.log(ans)
-    })
-    })
+            type: "input",
+            name: "inputNumber",
+            message: "How many units of this item would you like to purchase?",
+
+        }
+    ]).then(function(userPurchase) {
+        
+        connection.query("SELECT * FROM products WHERE id=?", userPurchase.inputId, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+
+            if (userPurchase.inputNumber > res[i].stock_quantity) {
+
+                console.log("===================================================");
+                console.log("Sorry! Not enough in stock. Please try again later.");
+                console.log("===================================================");
+
+                selectionPrompt();
+
+            } else {
+                //list item information for user for confirm prompt
+                console.log("===================================");
+                console.log("Your order can be placed.");
+                console.log("===================================");
+                console.log("You've selected:");
+                console.log("----------------");
+                console.log("Item: " + res[i].product_name);
+                console.log("Department: " + res[i].department_name);
+                console.log("Price: " + res[i].price);
+                console.log("Quantity: " + userPurchase.inputNumber);
+                console.log("----------------");
+                console.log("Total: " + res[i].price * userPurchase.inputNumber);
+                console.log("===================================");
+
+                var newStockamt = (res[i].stock_quantity - userPurchase.inputNumber);
+                var purchaseId = (userPurchase.inputId);
+                
+                confirmPrompt(newStockamt, purchaseId);
+                
+                
+            }
+        }
+    });
     
-}
-function checkQuant(){
-    connection.query("SELECT * FROM products", function(err, result){
-      
-        console.table(result);
+});
 
-        //prints stock quantity from table. 
-        // from the propmt we need the id and quantity of the product
-        inquirer
-    .prompt([
-        {
-      name: "id",
-      type: "input",
-      message: "What id?"
-    },
-    {
-        name: "quantity",
-        type: "input",
-        message: "how many?"
-      },
-    ]).then(function(ans){
-        console.log(ans)
-    })
-    })
 }
 
+function confirmPrompt(newStockamt, purchaseId) {
 
-// // function that compares quantities
-//function that checks the user out and adds up totals
-//in .then statements call those functions
+    inquirer.prompt([{
+
+        type: "confirm",
+        name: "confirmPurchase",
+        message: "Are you sure you would like to purchase this item and quantity?",
+        default: true
+
+    }]).then(function(userConfirm) {
+        if (userConfirm.confirmPurchase === true) {
+
+            connection.query("UPDATE products SET ? WHERE ?", [{
+                stock_quantity: newStockamt
+            }, {
+                item_id: purchaseId
+            }], function(err, res) {});
+
+            console.log("=================================");
+            console.log("Transaction completed. Thank you.");
+            console.log("=================================");
+            console.log("Thank you for coming. Goodbye!");
+            startPrompt();
+        } else {
+            console.log("=================================");
+            console.log("Are you sure? Ok, maybe next time. Have a nice day!");
+            console.log("=================================");
+            console.log("Thank you for coming. Goodbye!");
+            startPrompt();
+        }
+    });
+}
+
+
+        
+    
+
+   
